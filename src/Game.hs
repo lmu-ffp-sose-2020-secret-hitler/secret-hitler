d93162d704c65e81405043121eda356d097c0c42
rednaZ <p.rednaz@googlemail.com>
2020-08-26 00:51:50 +0200

    players as Map of alive player

--- src/Game.hs
+++ src/Game.hs
@@ -1,9 +1,10 @@
 module Game where
 
-import           Control.Lens
+import           Control.Lens hiding (element)
 import           Data.Maybe
 import GHC.Generics (Generic)
 import Data.Generics.Labels ()
+import Data.Map.Strict (Map)
 
 data Alignment = Good
                | Evil
@@ -16,32 +17,83 @@ data Role = GoodRole
 
 data Player = Player{
   role  :: Role,
-  vote  :: Maybe Bool,
-  alive :: Bool
+  vote  :: Maybe Bool
 } deriving stock (Show, Generic)
 
 data Policy = GoodPolicy
             | EvilPolicy
   deriving stock (Show)
 
+data Government =
+  Government
+    {
+      president :: PlayerId,
+      chancellor :: PlayerId
+    }
+  deriving stock (Show)
+
+data NominateChancellorPhaseData =
+  NominateChancellorPhaseData
+    {
+      governmentPrevious :: Maybe Government
+    }
+  deriving stock (Show)
+
 data VotePhaseData = VotePhaseData{
-  chancellorCandidate :: Int
+  governmentPrevious :: Maybe Government,
+  chancellorCandidate :: PlayerId
 } deriving stock (Show)
 
-data GamePhase = NominateChancellorPhase
+data PresidentDiscardPolicyPhaseData =
+  PresidentDiscardPolicyPhaseData
+    {
+      chancellor :: PlayerId
+    }
+  deriving stock (Show)
+
+data GamePhase = NominateChancellorPhase NominateChancellorPhaseData
                | VotePhase VotePhaseData
-               | PresidentDiscardPolicyPhase
+               | PresidentDiscardPolicyPhase PresidentDiscardPolicyPhaseData
                | ChancellorDiscardPolicyPhase
   deriving stock (Show)
 
+data PresidentTracker =
+  PresidentTracker {
+    president :: PlayerId,
+    regularPresidentLatest :: PlayerId
+  }
+  deriving stock (Show, Generic)
+
+newtype PlayerId =
+  PlayerId Int
+  deriving newtype (Show, Eq)
+
 data Game = Game{
   phase                 :: GamePhase,
-  players               :: [Player],
+  players               :: Map PlayerId Player,
+  deadPlayers :: Map PlayerId Player,
   drawPile              :: [Policy],
   goodPolicies          :: Int,
   evilPolicies          :: Int,
-  presidentialCandidate :: Int,
-  president             :: Maybe Int,
-  chancellor            :: Maybe Int,
+  presidentTracker :: PresidentTracker,
   electionTracker       :: Int
 } deriving stock (Show, Generic)
+
+succeedingElement :: Eq element => element -> [element] -> Maybe element
+succeedingElement element list =
+  listToMaybe $
+  drop 1 $
+  dropWhile (/= element) $
+  list ++ (take 1 list)
+
+passPresidencyRegularly :: [PlayerId] -> PresidentTracker -> Maybe PresidentTracker
+passPresidencyRegularly playerIds presidentTracker =
+  passPresidencyTo
+  <$>
+  (succeedingElement (presidentTracker ^. #regularPresidentLatest) playerIds)
+  where
+    passPresidencyTo :: PlayerId -> PresidentTracker
+    passPresidencyTo nextPresident =
+      set #president nextPresident $
+      set #regularPresidentLatest nextPresident $
+      presidentTracker

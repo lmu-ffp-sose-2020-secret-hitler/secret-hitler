e669e10d55d7fd64ec2c9e836f168ee96e056a84
Adrodoc <adrodoc55@googlemail.com>
2020-09-12 17:11:00 +0200

    Server sends game events to client

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -2,6 +2,7 @@ module Backend where
 
 import Common.GameMessages (
     GameAction (..),
+    GameEvent (..),
     GamePhase (..),
     GameView (..),
     PlayerView (..),
@@ -17,6 +18,7 @@ import Control.Monad
 import qualified Data.Aeson as Aeson
 import Data.ByteString.Lazy (toStrict)
 import Data.Dependent.Sum (DSum (..))
+import Data.Foldable (traverse_)
 import Data.Functor.Identity (Identity (Identity))
 import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IntMap
@@ -77,7 +79,8 @@ application stateMVar pending = do
               & #connections . at id .~ Just connection
               & #gameState .~ LobbyState lobbyNew
         putStrLn $ "Establishing connection to client " ++ show id
-        broadcast stateNew -- to-do. Are we fine with doing network IO while holding the mutex?
+        -- to-do. Are we fine with doing network IO while holding the mutex?
+        sendLobbyToAll (stateNew ^. #connections) lobbyNew
         return (stateNew, Just (id, connection))
       _ -> do
         WS.rejectRequest pending "Game is already in progress"
@@ -96,36 +99,114 @@ removeClient :: Int -> MVar ServerState -> IO ()
 removeClient id stateMVar = do
   putStrLn $ "Client " ++ show id ++ " disconnected"
   modifyMVar_ stateMVar $ \stateOld -> do
-    let stateNew = stateOld
-          & #connections . at id .~ Nothing
-          & #gameState %~ removeClientFromLobby id
-    broadcast stateNew -- to-do. Are we fine with doing network IO while holding the mutex?
-    return stateNew
+    let stateNew = stateOld & #connections . at id .~ Nothing
+    -- to-do. Are we fine with doing network IO while holding the mutex?
+    removeClientFromLobby id stateNew
 
-removeClientFromLobby :: Int -> GameState -> GameState
-removeClientFromLobby id (LobbyState lobby) =
-  LobbyState $ Lobby.removePlayer id lobby
-removeClientFromLobby _id gameState = gameState
+removeClientFromLobby :: Int -> ServerState -> IO ServerState
+removeClientFromLobby id state@(ServerState {
+  connections,
+  gameState = LobbyState lobbyOld
+}) = do
+  let lobbyNew = Lobby.removePlayer id lobbyOld
+  sendLobbyToAll connections lobbyNew
+  return $ state & #gameState .~ LobbyState lobbyNew
+removeClientFromLobby _id state = return state
 
-broadcast :: ServerState -> IO ()
-broadcast (ServerState {connections, gameState}) = do
-  _ <- IntMap.traverseWithKey (sendState gameState) connections
-  return ()
+talk :: Int -> WS.Connection -> MVar ServerState -> IO ()
+talk id connection stateMVar = do
+  putStrLn $ "Client " ++ show id ++ " connected"
+  forever $ do
+    messageMaybe <- Aeson.decodeStrict' <$> WS.receiveData connection
+    case messageMaybe of
+      Nothing -> putStrLn $ "Could not decode message from client " ++ show id
+      Just message -> do
+        Text.putStrLn $ "Received message from client " <> Text.pack (show id) <>
+          ": " <> (decodeUtf8 $ toStrict $ Aeson.encode message)
+        case message of
+          LobbyAction payload -> modifyMVar_ stateMVar (answerLobbyToServer id payload)
+          GameAction payload -> modifyMVar_ stateMVar (answerGameToServer id payload)
 
-sendState :: GameState -> Int -> WS.Connection -> IO ()
-sendState gameState playerId = sendMessage $ stateMessage gameState playerId
+sendToAll :: (Foldable f, Aeson.ToJSON msg) => f WS.Connection -> msg -> IO ()
+sendToAll connections message =
+  traverse_ (sendMessage message) connections
+
+sendToAllWithKey :: Aeson.ToJSON msg => IntMap WS.Connection -> (Int -> msg) -> IO ()
+sendToAllWithKey connections createMessage =
+  void $ IntMap.traverseWithKey (sendMessage . createMessage) connections
 
 sendMessage :: Aeson.ToJSON msg => msg -> WS.Connection -> IO ()
 sendMessage message connection = WS.sendTextData connection $ Aeson.encode message
 
-stateMessage :: GameState -> Int -> StateFromServer
-stateMessage (LobbyState lobby) _playerId = LobbyFromServer $ lobbyView lobby
-stateMessage (GameState game) playerId = GameFromServer $ gameView game playerId
+----------------------------------------------------------------------------------------------------
+--    _            _      _
+--   | |     ___  | |__  | |__   _   _
+--   | |    / _ \ | '_ \ | '_ \ | | | |
+--   | |___| (_) || |_) || |_) || |_| |
+--   |_____|\___/ |_.__/ |_.__/  \__, |
+--                               |___/
+----------------------------------------------------------------------------------------------------
+
+answerLobbyToServer :: Int -> LobbyAction -> ServerState -> IO (ServerState)
+answerLobbyToServer id payload stateOld@ServerState {
+  connections,
+  gameState = LobbyState lobbyOld
+} = do
+  stateNew <- case payload of
+    StartGame -> do
+      let playerNames = lobbyOld ^. #players <&> view #name
+      game <- runRandomIO $ Game.generateRandomGame playerNames
+      sendToAllWithKey connections (gameMessage game Nothing)
+      return $ stateOld & #gameState .~ GameState game
+    Join nameNew -> do
+      let lobbyNew = lobbyOld & #players . ix id . #name .~ nameNew
+      -- to-do. Are we fine with doing network IO while holding the mutex?
+      sendLobbyToAll connections lobbyNew
+      return $ stateOld & #gameState .~ LobbyState lobbyNew
+  return stateNew
+answerLobbyToServer _id _payload stateOld = do
+  putStrLn "There is currently no active Lobby"
+  return stateOld
+
+sendLobbyToAll :: IntMap WS.Connection -> Lobby -> IO ()
+sendLobbyToAll connections lobby =
+  sendToAll connections (lobbyMessage lobby)
+
+lobbyMessage :: Lobby -> StateFromServer
+lobbyMessage lobby = LobbyFromServer $ lobbyView lobby
 
 lobbyView :: Lobby -> LobbyView
 lobbyView (Lobby {players}) = LobbyView {
   playerNames = fmap (view #name) $ IntMap.elems $ players
 }
+
+----------------------------------------------------------------------------------------------------
+--     ____
+--    / ___|  __ _  _ __ ___    ___
+--   | |  _  / _` || '_ ` _ \  / _ \
+--   | |_| || (_| || | | | | ||  __/
+--    \____| \__,_||_| |_| |_| \___|
+--
+----------------------------------------------------------------------------------------------------
+
+answerGameToServer :: Int -> GameAction -> ServerState -> IO (ServerState)
+answerGameToServer id payload stateOld@ServerState {
+  connections,
+  gameState = GameState gameOld
+} = do
+  (gameNew, gameEvent) <- runRandomIO $ Game.updateChecked id payload gameOld
+  let stateNew = stateOld & #gameState .~ GameState gameNew
+  -- to-do. Are we fine with doing network IO while holding the mutex?
+  sendToAllWithKey connections (gameMessage gameNew (Just gameEvent))
+  return stateNew
+answerGameToServer _id _payload stateOld = do
+  putStrLn "There is currently no Game in progress"
+  return stateOld
+
+gameMessage :: Game -> Maybe GameEvent -> Int -> StateFromServer
+gameMessage game event playerId =
+  GameFromServer $
+  GameUpdate (gameView game playerId) (event)
 
 gameView :: Game -> Int -> GameView
 gameView game@(Game {
@@ -178,43 +259,3 @@ playerView playerRole playerCount gamePhase (Game.Player {
     canSeeOtherRoles GoodRole _playerCount = False
     canSeeOtherRoles EvilRole _playerCount = True
     canSeeOtherRoles EvilLeaderRole playerCount = playerCount <= 5
-
-talk :: Int -> WS.Connection -> MVar ServerState -> IO ()
-talk id connection stateMVar = do
-  putStrLn $ "Client " ++ show id ++ " connected"
-  forever $ do
-    messageMaybe <- Aeson.decodeStrict' <$> WS.receiveData connection
-    case messageMaybe of
-      Nothing -> putStrLn $ "Could not decode message from client " ++ show id
-      Just message -> do
-        Text.putStrLn $ "Received message from client " <> Text.pack (show id) <>
-          ": " <> (decodeUtf8 $ toStrict $ Aeson.encode message)
-        case message of
-          LobbyAction payload -> modifyMVar_ stateMVar (answerLobbyToServer id payload)
-          GameAction payload -> modifyMVar_ stateMVar (answerGameToServer id payload)
-
-answerLobbyToServer :: Int -> LobbyAction -> ServerState -> IO (ServerState)
-answerLobbyToServer id payload stateOld@ServerState {gameState=LobbyState lobbyOld} = do
-  stateNew <- case payload of
-    StartGame -> do
-      let playerNames = lobbyOld ^. #players <&> view #name
-      game <- runRandomIO $ Game.generateRandomGame playerNames
-      return $ stateOld & #gameState .~ GameState game
-    Join nameNew ->
-      let lobbyNew = lobbyOld & #players . ix id . #name .~ nameNew in
-      return $ stateOld & #gameState .~ LobbyState lobbyNew
-  broadcast stateNew -- to-do. Are we fine with doing network IO while holding the mutex?
-  return stateNew
-answerLobbyToServer _id _payload stateOld = do
-  putStrLn "There is currently no active Lobby"
-  return stateOld
-
-answerGameToServer :: Int -> GameAction -> ServerState -> IO (ServerState)
-answerGameToServer id payload stateOld@ServerState {gameState=GameState gameOld} = do
-  (gameNew, _) <- runRandomIO $ Game.updateChecked id payload gameOld
-  let stateNew = stateOld & #gameState .~ GameState gameNew
-  broadcast stateNew -- to-do. Are we fine with doing network IO while holding the mutex?
-  return stateNew
-answerGameToServer _id _payload stateOld = do
-  putStrLn "There is currently no Game in progress"
-  return stateOld
--- common/src/Common/MessageTypes.hs
+++ common/src/Common/MessageTypes.hs
@@ -2,7 +2,7 @@
 
 module Common.MessageTypes where
 
-import Common.GameMessages (GameView, GameAction)
+import Common.GameMessages (GameAction, GameEvent, GameView)
 import Data.Text (Text)
 import Data.Aeson as A
 import GHC.Generics (Generic)
@@ -12,43 +12,45 @@ import Data.Dependent.Sum (DSum ((:=>)), (==>))
 
 data StateFromServer =
   LobbyFromServer LobbyView |
-  GameFromServer GameView
+  GameFromServer GameUpdate
   deriving stock (Generic)
-instance ToJSON StateFromServer where
-  toEncoding = genericToEncoding defaultOptions
 instance FromJSON StateFromServer
+instance ToJSON StateFromServer where
 
 data LobbyView =
   LobbyView {
     playerNames :: [Text]
   }
   deriving stock (Generic)
-instance ToJSON LobbyView where
-  toEncoding = genericToEncoding defaultOptions
 instance FromJSON LobbyView
+instance ToJSON LobbyView where
 
 lobbyViewInitial :: LobbyView
 lobbyViewInitial = LobbyView []
+
+data GameUpdate =
+  GameUpdate GameView (Maybe GameEvent)
+  deriving stock (Generic)
+instance FromJSON GameUpdate
+instance ToJSON GameUpdate where
 
 data ActionFromClient =
   LobbyAction LobbyAction |
   GameAction GameAction
   deriving stock (Generic)
-instance ToJSON ActionFromClient where
-  toEncoding = genericToEncoding defaultOptions
 instance FromJSON ActionFromClient
+instance ToJSON ActionFromClient where
 
 data LobbyAction =
   Join Text |
   StartGame
   deriving stock (Show, Generic)
-instance ToJSON LobbyAction where
-  toEncoding = genericToEncoding defaultOptions
 instance FromJSON LobbyAction
+instance ToJSON LobbyAction where
 
 data StateFromServerTag a where
   LobbyFromServerTag :: StateFromServerTag LobbyView
-  GameFromServerTag :: StateFromServerTag GameView
+  GameFromServerTag :: StateFromServerTag GameUpdate
 
 instance GEq StateFromServerTag where
   geq LobbyFromServerTag LobbyFromServerTag = Just Refl
@@ -59,10 +61,10 @@ stateFromServerToDSum :: StateFromServer -> DSum StateFromServerTag Identity
 stateFromServerToDSum =
   \case
     LobbyFromServer lobbyView -> LobbyFromServerTag ==> lobbyView
-    GameFromServer gameView -> GameFromServerTag ==> gameView
+    GameFromServer gameUpdate -> GameFromServerTag ==> gameUpdate
     
 dsumToStateFromServer :: DSum StateFromServerTag Identity -> StateFromServer
 dsumToStateFromServer =
   \case
     LobbyFromServerTag :=> Identity lobbyView -> LobbyFromServer lobbyView
-    GameFromServerTag :=> Identity gameView -> GameFromServer gameView
+    GameFromServerTag :=> Identity gameUpdate -> GameFromServer gameUpdate

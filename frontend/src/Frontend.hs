e541eeaddb622d3622a78b143aa6c980becee62d
rednaZ <p.rednaz@googlemail.com>
2020-10-08 23:53:11 +0200

    Revert "Generalize collapseComposeIdentity from Dynamic to Functor"
    This reverts commit d38d93d2b757471397ef5de1df052c7e121be77f.

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -2,7 +2,6 @@ module Frontend where
 
 import Common.GameMessages
 import Data.Char (toUpper)
-import Data.Coerce (Coercible, coerce)
 import Data.Foldable (for_)
 import Data.Traversable (for)
 import Control.Monad.Fix (MonadFix)
@@ -646,8 +645,9 @@ stateFromServerDyn ::
 stateFromServerDyn =
   (fmap . fmap) collapseComposeIdentity . factorDyn . fmap stateFromServerToDSum
 
-collapseComposeIdentity :: Functor f => DSum tag (Compose f Identity) -> DSum tag f
-collapseComposeIdentity = mapNaturalTransformer (fmap coerce . getCompose)
+collapseComposeIdentity ::
+  Reflex t => DSum tag (Compose (Dynamic t) Identity) -> DSum tag (Dynamic t)
+collapseComposeIdentity = mapNaturalTransformer (coerceDynamic . getCompose)
 
 mapNaturalTransformer :: (forall a. f a -> g a) -> DSum k f -> DSum k g
 mapNaturalTransformer f (sumTag :=> v) = sumTag :=> f v
